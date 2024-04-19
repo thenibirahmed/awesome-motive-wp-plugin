d08451640aed3d5fe92c91a52f1d0048a8f874c0
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-04-20 01:41:32 +0600

    refactored some frontend codes

--- includes/CLI.php
+++ includes/CLI.php
@@ -2,8 +2,6 @@
 
 namespace AwesomeMotive;
 
-use AwesomeMotive\CLI\DeleteTableBlockData;
-
 class CLI {
     public function __construct() {
         if ( class_exists( 'WP_CLI' ) ) {
@@ -11,6 +9,11 @@ class CLI {
         }
     }
 
+    /**
+     * Register the WP CLI commands
+     *
+     * @return void
+     */
     public function register_commands() {
         \WP_CLI::add_command( 'delete-table-block-data', 'AwesomeMotive\CLI\DeleteTableBlockData' );
     }
---
+++ includes/Frontend/TableBlock.php
@@ -0,0 +1,65 @@
+<?php 
+
+namespace AwesomeMotive\Frontend;
+
+use AwesomeMotive\Ajax;
+
+class TableBlock {
+    public function __construct() {
+        add_action( 'init', [ $this, 'register_block' ]);
+    }
+
+    public function register_block() {
+        register_block_type( 'awesome-motive/table-block', [
+            'editor_script' => 'awesome-motive-block-script',
+            'editor_style'  => 'awesome-motive-block-style',
+            'render_callback' => [ $this, 'render_am_table_block' ]
+        ]);
+    }
+
+    public function render_am_table_block( $attributes ) {
+        $api_data = Ajax::fetch_table_data_from_api();
+
+        if ( ! $api_data ) {
+            return __('Error fetching data', 'awesome-motive');
+        }
+        
+        $table_data = json_decode( $api_data );
+
+        $title = $table_data->title ?? __('Table', 'awesome-motive');
+        $headers = $table_data->data->headers ?? [];
+        $rows = $table_data->data->rows ?? [];
+        
+        $hidden_columns = $attributes['hiddenColumns'] ?? [];
+        ob_start();
+        ?>
+        <div class="awesome-motive-table-block">
+            <h4><?php echo esc_html($title) ?></h4>
+            <table>
+                <thead>
+                    <tr>
+                        <?php foreach( $headers as $header ): ?>
+                            <?php echo !in_array($header, $hidden_columns) ? "<th>". esc_html($header) ."</th>" : "" ?>
+                        <?php endforeach ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach( $rows as $row ) : ?>
+                        <tr>
+                            <?php 
+                            $i = 0;
+                            foreach( $row as $cell ) : ?>
+                                <?php 
+                                    echo !in_array($headers[$i], $hidden_columns) ? "<td>". esc_html($cell) ."</td>" : "";
+                                    $i++;
+                                ?>
+                            <?php endforeach; ?>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+        <?php
+        return ob_get_clean();
+    }
+}
