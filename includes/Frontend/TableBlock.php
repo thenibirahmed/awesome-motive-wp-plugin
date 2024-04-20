daae2306cdff5456dedf536d4d68196a24f51597
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-04-20 21:30:01 +0600

    table blcok UI refactored

--- includes/Frontend/TableBlock.php
+++ includes/Frontend/TableBlock.php
@@ -31,35 +31,11 @@ class TableBlock {
         $rows = $table_data->data->rows ?? [];
         
         $hidden_columns = $attributes['hiddenColumns'] ?? [];
+
         ob_start();
-        ?>
-        <div class="awesome-motive-table-block">
-            <h4><?php echo esc_html($title) ?></h4>
-            <table>
-                <thead>
-                    <tr>
-                        <?php foreach( $headers as $header ): ?>
-                            <?php echo !in_array($header, $hidden_columns) ? "<th>". esc_html($header) ."</th>" : "" ?>
-                        <?php endforeach ?>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach( $rows as $row ) : ?>
-                        <tr>
-                            <?php 
-                            $i = 0;
-                            foreach( $row as $cell ) : ?>
-                                <?php 
-                                    echo !in_array($headers[$i], $hidden_columns) ? "<td>". esc_html($cell) ."</td>" : "";
-                                    $i++;
-                                ?>
-                            <?php endforeach; ?>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
-        <?php
+
+        require_once AWESOME_MOTIVE_PATH . '/includes/views/table-block-frontend.php';
+        
         return ob_get_clean();
     }
 }
